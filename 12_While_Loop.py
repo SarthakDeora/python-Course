aa81a4bb7b710345fb83f9dfcd2c0ec8c8945b3a
Sarthak <sarthakdeora@gmail.com>
2020-12-24 19:41:23 +0530

    Initial commit

---
+++ 12_While_Loop.py
@@ -0,0 +1,8 @@
+# While loop is easy!!!
+
+# First I will make a variable
+i = 1
+# then i will satrt a while loop
+while (i < 45):  # This is how we make a while loop it's super easy.  Youu have to type a condition in the  brackets
+    print(i)  # then the code which will execute whenever the condition is true
+    i = i+1
